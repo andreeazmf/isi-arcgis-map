c62b45451bc8a57e280f091213a58153f9211f8b
andreeazmf <andreeazamfirescu00@gmail.com>
2021-11-10 20:15:29 +0200

    added my firebase project configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,6 +11,12 @@
 export const environment = {
   firebase: {
     // add your Firebase config here
+    apiKey: "AIzaSyA2sw_A9YmpTo-R2FT-weQurQmA53t8eWo",
+    authDomain: "isi-lab-5.firebaseapp.com",
+    projectId: "isi-lab-5",
+    storageBucket: "isi-lab-5.appspot.com",
+    messagingSenderId: "806639664683",
+    appId: "1:806639664683:web:2cd3b16e582d9b531ab46e"
   },
   production: false,
 };
